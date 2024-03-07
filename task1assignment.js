cfa6df8061c4a5fe77ca73bf5983c6ef8859d68e
shree-varshaa-k <156644283+shree-varshaa-k@users.noreply.github.com>
2024-03-06 21:37:44 -0800

    javascripttasks

---
+++ task1assignment.js
@@ -0,0 +1,126 @@
+//values and variables
+
+// var country ="america";
+// var continent ="asia";
+// var population= 4 ;
+// console.log(country,continent,population);
+
+//datatypes
+
+// let island = false;
+// let india=true;
+// let language;
+
+// console.log(typeof island);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// console.log(india);
+
+//let,const and var
+// const language="tamil";
+// language="english";
+// console.log(language);
+
+//basic operators
+// var population=10;
+// var country= "portugal";
+// console.log(country,population);
+// population++;
+// console.log(population);
+// var finlandpopulation = 6;
+// if (country > finlandpopulation){
+//     console.log("country have more people than finland")
+// }
+// else{
+//     console.log("finlnd have more people than finland")
+// }
+// var averagepopulation=33;
+// if(population < averagepopulation ){
+
+//     console.log("population is less than average population")
+// }
+// else{
+//     console.log("population is greater than average population")
+// }
+// var description="portugal is in europe, and its 11million people speak portuguese";
+// console.log(description);
+
+//if/else statement
+
+// var population= 30;
+// var averagepopulation=33;
+// var average= averagepopulation-population;
+// if (population > averagepopulation){
+//     console.log("portugal's population is above average")
+// }
+// else{
+//     console.log(`portugal's population is ${average} million below average`)
+// }
+
+//type conversion and coercion
+
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 +5 + '4' +9-4-2);
+
+//equality operator == vs ===
+// var numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+// if (numNeighbours == 1) {
+//     console.log("Only 1 border!");
+// } 
+// else if (numNeighbours > 1) {
+//     console.log("More than 1 border");
+// } 
+// else {
+//     console.log("No borders");
+// }
+
+// var numNeighbours = prompt('how many neighbours countries does your country have?');
+// console.log(typeof numNeighbours)
+// if(numNeighbours === 1)
+// {
+//     console.log("only 1 border!");
+// }
+// else if(numNeighbours > 1)
+// {
+//     console.log("more than 1 border");
+// }
+// else{
+//     console.log("no borders");
+// }
+//switch statement
+
+// var language = "chinese";
+// switch(language){
+//     case "chinese":
+//     console.log("most number of native speakers!");
+//     break;
+//     case "spanish":
+//     console.log("2nd place in number of native speaker's");
+//     break;
+//     case "english":
+//     console.log("3rd place");
+//     break;
+//     case "hindi":
+//     console.log("number 4");
+//     break;
+//     case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//     case "for all other simply":
+//     console.log("great language too");
+//     break;
+// }
+
+//The conditional ternary operator
+// var population = 40;
+// var aboveaveragemeans = population > 33 ? "portugals population is above averge" : "portugals population is below average"
+// console.log(aboveaveragemeans)
+
+
+
---
+++ task1coding.js
@@ -0,0 +1,75 @@
+// // Assignment 1
+ 
+// Coding Challenge #1
+
+// var massMark = 78;
+// var heightMark = 1.67;
+
+// var massJohn = 85;
+// var heightJohn = 1.55;
+
+// var BMIMark = massMark / (heightMark * heightMark);
+// var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// console.log(BMIMark, BMIJohn);
+  
+// var markHigherBMI = BMIMark < BMIJohn;
+// console.log("Is mark has a higher BMI than BMI John : ", markHigherBMI);
+
+
+
+// Coding Challenge #2
+
+    // if(BMIMark > BMIJohn){
+    //     console.log("Mark's BMI is higher than John's!");
+    // }
+    // else{
+    //     console.log("John's BMI is higher than Mark's!");
+    // }
+
+    // if(BMIMark > BMIJohn){
+    //     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+    // }
+    // else{
+    //     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+    // }
+
+
+
+// Coding Challenge #3
+
+    // var dolphin = [97, 112, 101];
+    // var koala = [96, 108, 89];
+    // var sum = 0;
+    // for(let i of dolphin){
+    //     dolphinSum = (sum + i) / dolphin.length;
+    // }
+    // for(let i of koala){
+    //     koalaSum = (sum + i) / koala.length;
+    // }
+    //                 // for(let j =0; j < dolphins.length; j++){
+    //                 //     sum += dolphins[j];
+    //                 // }
+    // console.log(dolphinSum);
+    // console.log(koalaSum);
+
+    // if(dolphinSum > koalaSum){
+    //     console.log("Dolphin win the trophy");
+    // }
+    // else if(dolphinSum < koalaSum){
+    //     console.log("Koalas win the trophy");
+    // }
+    // else{
+    //     console.log("Both win the trophy")
+    // }
+
+
+
+
+// Coding Challenge #4
+
+    // var bill = 275;
+    // var tip = 50 <= bill && bill <= 300 
+    //             ? `The bill was ${bill}, the tip was ${0.15 * bill}, and the total value is ${bill + (0.15 * bill)}` 
+    //             : `The bill was ${bill}, the tip was ${0.20 * bill}, and the total value is ${bill + (0.20 * bill)}`
+    // console.log(tip);
